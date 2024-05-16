4599e3757c0df543fbcb11c93ce88f11b304dfe4
Ondřej Ondryáš <ondryaso@ondryaso.eu>
2024-05-16 20:20:34 +0200

    Add more "audit-y" logging

--- python_pipeline/common/audit.py
+++ python_pipeline/common/audit.py
@@ -1,9 +1,13 @@
 import logging
+from pprint import pprint
 
 logger = logging.getLogger(__name__)
 
 
 def log_unhandled_error(e: Exception, component_id: str, key: str | None = None, **kwargs):
     # TODO: Robust error handling...
-    logger.error(f"Error processing {component_id} data for key {key}", exc_info=e, stack_info=True,
+    logger.error(f"Error processing {component_id} data" + "" if not key else f" key {key}",
+                 exc_info=e,
+                 stack_info=True,
                  extra={"component": component_id, "key": key, **kwargs})
+    logger.error(pprint(kwargs))
