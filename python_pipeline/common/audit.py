05d73854d175cf50fd249c0d6ae2a28c4ea9fae1
Ondřej Ondryáš <ondryaso@ondryaso.eu>
2024-06-05 12:19:01 +0200

    Enhance logging

--- python_pipeline/common/audit.py
+++ python_pipeline/common/audit.py
@@ -1,18 +1,34 @@
 import logging
-from pprint import pprint
+import sys
+from pprint import pformat
+
+
+class ExtraFormatter(logging.Formatter):
+    def __init__(self):
+        super().__init__(fmt="[%(asctime)s] [%(levelname)s] [%(component)s#%(threadName)s] {%(event_key)s} %(message)s", style='%')
+
+    def formatMessage(self, record):
+        formatted = super().formatMessage(record)
+        if hasattr(record, "properties"):
+            properties = record.properties
+            if properties is not None and len(properties) > 0:
+                formatted += "\nProperties: " + pformat(properties)
+        return formatted
+
 
 logger = logging.getLogger(__name__)
+handler = logging.StreamHandler(sys.stderr)
+handler.setLevel(logging.INFO)
+formatter = ExtraFormatter()
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 
 def log_warning(component_id: str, message: str, key: str | None = None, **kwargs):
-    logger.warning(f"{component_id} warning: " + ("" if not key else f"[{key}] ") + message,
-                   extra={"component": component_id, "key": key, **kwargs})
+    logger.warning("Warning: " + message,
+                   extra={"component": component_id, "event_key": key or "no key", "properties": kwargs})
 
 
 def log_unhandled_error(e: Exception, component_id: str, key: str | None = None, **kwargs):
-    # TODO: Robust error handling...
-    logger.error(f"{component_id} unexpected error" + "" if not key else f" [{key}]",
-                 exc_info=e,
-                 stack_info=True,
-                 extra={"component": component_id, "key": key, **kwargs})
-    logger.error(pprint(kwargs))
+    logger.error("Unexpected error:", exc_info=e, stack_info=True,
+                 extra={"component": component_id, "event_key": key or "no key", "properties": kwargs})
