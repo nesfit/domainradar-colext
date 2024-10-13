a1b271e1444ddcaa6d17f6239dd7ef302d971ae3
Ondřej Ondryáš <ondryaso@ondryaso.eu>
2024-10-13 13:26:49 +0200

    Add Connect infrastructure for storing collection results in Postgres

---
+++ java_pipeline/connect/src/main/java/cz/vut/fit/domainradar/connect/PostgresSinkTransformation.java
@@ -0,0 +1,121 @@
+package cz.vut.fit.domainradar.connect;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import cz.vut.fit.domainradar.Common;
+import cz.vut.fit.domainradar.Topics;
+import cz.vut.fit.domainradar.models.IPToProcess;
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.connect.connector.ConnectRecord;
+import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.connect.data.SchemaBuilder;
+import org.apache.kafka.connect.data.Struct;
+import org.apache.kafka.connect.data.Timestamp;
+import org.apache.kafka.connect.errors.DataException;
+import org.apache.kafka.connect.transforms.Transformation;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.time.Instant;
+import java.util.Map;
+
+/**
+ * Implementation of {@link Transformation} that produces records suitable for the PostgreSQL sink connector.
+ * <p>
+ * The transformation expects collector result entries (both DN- and IP-based) in their serialized form. It deserializes
+ * both the key and the value and outputs a new record to be ingested by the properly configured sink connector which
+ * inserts the record into the table for collection results.
+ * <p>
+ * The key follows {@link #KEY_SCHEMA} and contains the "primary key" fields – DN, IP, collector name and timestamp.
+ * The IP is {@code null} if the result comes from a DN-based collector.
+ * <p>
+ * The value follows {@link #VALUE_SCHEMA} that contains the status code, the error message and the raw result data,
+ * i.e. the entire JSON string with the result.
+ * @author Ondřej Ondryáš
+ */
+public class PostgresSinkTransformation<R extends ConnectRecord<R>> implements Transformation<R> {
+    public record CommonResult(
+            int statusCode,
+            @Nullable String error,
+            @NotNull Instant lastAttempt,
+            @Nullable String collector
+    ) {
+    }
+
+    public static final Schema KEY_SCHEMA = SchemaBuilder.struct()
+            .name("cz.vut.fit.domainradar.PostgresCollectedDataKey")
+            .field("domain_name", Schema.STRING_SCHEMA)
+            .field("ip", Schema.OPTIONAL_STRING_SCHEMA)
+            .field("collector", Schema.STRING_SCHEMA)
+            .field("timestamp", Timestamp.SCHEMA);
+
+    public static final Schema VALUE_SCHEMA = SchemaBuilder.struct()
+            .name("cz.vut.fit.domainradar.PostgresCollectedDataValue")
+            .field("status_code", Schema.INT16_SCHEMA)
+            .field("error", Schema.OPTIONAL_STRING_SCHEMA)
+            .field("raw_data", Schema.OPTIONAL_STRING_SCHEMA);
+    private final ObjectMapper _mapper;
+
+    public PostgresSinkTransformation() {
+        _mapper = Common.makeMapper().build();
+    }
+
+    @Override
+    public R apply(R record) {
+        if (record.keySchema().type() != Schema.Type.BYTES
+                || record.valueSchema().type() != Schema.Type.BYTES)
+            throw new DataException("Expected the BYTES schema for both the key and the value");
+
+        final var topic = record.topic();
+        final var isIpResult = topic.equals(Topics.OUT_IP);
+        final var key = (byte[]) record.key();
+        final var value = (byte[]) record.value();
+
+        try {
+            final var result = _mapper.readValue(value, CommonResult.class);
+
+            String domainName, ip, collector;
+            if (isIpResult) {
+                var ipData = _mapper.readValue(key, IPToProcess.class);
+                domainName = ipData.dn();
+                ip = ipData.ip();
+                collector = result.collector;
+            } else {
+                domainName = new String(key, StandardCharsets.UTF_8);
+                ip = null;
+                collector = Topics.TOPICS_TO_COLLECTOR_ID.get(topic);
+            }
+
+            final var keyStruct = new Struct(KEY_SCHEMA);
+            final var valueStruct = new Struct(VALUE_SCHEMA);
+
+            keyStruct.put("domain_name", domainName);
+            keyStruct.put("ip", ip);
+            keyStruct.put("collector", collector);
+            keyStruct.put("timestamp", java.util.Date.from(result.lastAttempt()));
+
+            valueStruct.put("status_code", (short) result.statusCode);
+            valueStruct.put("error", result.error);
+            valueStruct.put("raw_data", new String(value, StandardCharsets.UTF_8));
+
+            return record.newRecord(topic, record.kafkaPartition(), KEY_SCHEMA, keyStruct, VALUE_SCHEMA, valueStruct,
+                    record.timestamp());
+        } catch (IOException e) {
+            throw new DataException("Failed to read topic message", e);
+        }
+    }
+
+    @Override
+    public void close() {
+    }
+
+    @Override
+    public ConfigDef config() {
+        return new ConfigDef();
+    }
+
+    @Override
+    public void configure(Map<String, ?> configs) {
+    }
+}
